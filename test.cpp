f8cc44dc16a83b584cd78387d42e747f9593e987
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-02-01 17:58:38 +0300

    Update test.cpp
    Сохранение собирающегося варианта.

--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
 #define BOOST_TEST_MODULE BayanTests
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(test_example) {
     BOOST_TEST(1 + 1 == 2);
 }
+
 
 
 
