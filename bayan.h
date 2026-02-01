a01635a23efe9b6a31ce63580c43e336e08ed4bf
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-02-01 18:00:20 +0300

    Update bayan.h
    сохранение.

--- bayan.h
+++ bayan.h
@@ -5,6 +5,12 @@
 #include <iostream>
 
 
+struct BayanConfig {
+    std::vector<std::string> directories;
+    std::vector<std::string> exclude_dirs;
+    int scan_depth = -1;
+    size_t min_size = 1;
+    
+};
 
-void bayan_parser(size_t n, const std::string& currCmd);
-
+void bayan_parser(BayanConfig& conf);
