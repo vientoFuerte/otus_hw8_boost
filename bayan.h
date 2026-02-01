b657a4e59fd91441f5fc6753c1179980df91254b
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-02-01 20:57:49 +0300

    Update bayan.h
    Добавлены все поля по тз.

--- bayan.h
+++ bayan.h
@@ -8,9 +8,15 @@
 struct BayanConfig {
     std::vector<std::string> directories;
     std::vector<std::string> exclude_dirs;
+    std::vector<std::string> file_masks;      // Маски файлов (например, *.txt, *.cpp)
+    std::string hash_algorithm = "md5"; 
     int scan_depth = -1;
-    size_t min_size = 1;
+    size_t min_size = 1;                      // Минимальный размер файла (по умолчанию 1 байт)
+    size_t block_size = 1024;                 // Размер блока для чтения файлов
+     
     
 };
 
 void bayan_parser(BayanConfig& conf);
+
+
