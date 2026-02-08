3430aa1779f36604615e94ec9b9810f9b7d30a3f
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-02-08 21:37:32 +0300

    Update bayan.h
    Вынесены все подключения библиотек и определения функций.

--- bayan.h
+++ bayan.h
@@ -24,10 +24,22 @@ struct BayanConfig {
     std::vector<std::string> directories;
     std::vector<std::string> exclude_dirs;
     std::vector<std::string> file_masks;      // Маски файлов (например, *.txt, *.cpp)
-    std::string hash_algorithm = "md5"; 
+    std::string hash_algorithm = "crc32"; 
     int scan_depth = -1;
     size_t min_size = 1;                      // Минимальный размер файла (по умолчанию 1 байт)
-    size_t block_size = 1024;                 // Размер блока для чтения файлов  
+    size_t block_size = 1024;                 // Размер блока для чтения файлов
+  
 };
 
 
+// Функция для полного сравнения файлов по всем блокам
+bool areFilesIdentical(const FileInfo& fi1, const FileInfo& fi2, size_t block_size);
+
+// Функция для сравнения двух файлов одного размера по конкретному блоку
+bool compareSingleBlock(const FileInfo& fi1, const FileInfo& fi2, size_t block_size, size_t blockNum);
+
+// Функция для выделения групп файлов одного размера
+std::vector<std::vector<FileInfo>> extractSameSizeGroups(std::vector<FileInfo>& finfo);
+
+// Функция сбора файлов
+ std::vector<FileInfo> collectFiles(const BayanConfig& conf);
